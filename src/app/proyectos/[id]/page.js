abc2a4a695ed477964678f4629d730a21d23ed90
infinn <71609708+infinn@users.noreply.github.com>
2024-01-16 23:27:23 -0400

    Update page.js

--- src/app/proyectos/[id]/page.js
+++ src/app/proyectos/[id]/page.js
@@ -1,23 +1,7 @@
 import  CreadorPost  from "../../components/creador-post/page"
 import "./style.css"
 
-
-/*
-export const generateStaticParams = async()=>{
-    const data = await fetch("/proyectos", {
-        cache:"no-store",
-    });
-    const qdata = await data.json();
-    const das = qdata.proyectos;
-    const params = das.map((proyectos)=>{
-        id:{proyectos.id}
-    })
-    console.log(params)
-    return params;
-}
-
 export default function Page({ params }){
-    const { id } = params
     return(
         <>
             
@@ -28,24 +12,4 @@ export default function Page({ params }){
         
     )
 }
-*/
-// Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams() {
-    const posts = await fetch('https://.../posts').then((res) => res.json())
-   
-    return posts.map((post) => ({
-      id: post.id,
-    }))
-  }
-   
-  // Multiple versions of this page will be statically generated
-  // using the `params` returned by `generateStaticParams`
-  export default function Page({ params }) {
-    const { id } = params
-    return(
-        <>
-        <h1>{ params }</h1>
-        </>
-    )
-    // ...
-  }
+
