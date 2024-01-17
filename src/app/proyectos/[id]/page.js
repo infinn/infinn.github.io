7fbef42c2da319e8510e6122a51d58fb6bff8d6e
infinn <71609708+infinn@users.noreply.github.com>
2024-01-17 10:08:30 -0400

    Update page.js

--- src/app/proyectos/[id]/page.js
+++ src/app/proyectos/[id]/page.js
@@ -1,7 +1,15 @@
 import  CreadorPost  from "../../components/creador-post/page"
 import "./style.css"
 
+export async function generateStaticParams() {
+  const posts = await fetch('https://infinn.github.io/proyectos').then((res) => res.json())
+ 
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
+}
 export default function Page({ params }){
+    const { slug } = params
     return(
         <>
             
