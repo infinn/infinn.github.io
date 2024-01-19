c3939a7c5e35fc35edf78c97851884716532506d
infinn <71609708+infinn@users.noreply.github.com>
2024-01-19 19:26:17 -0400

    Update bl.js

--- src/app/components/bl-layout/bl.js
+++ src/app/components/bl-layout/bl.js
@@ -13,20 +13,23 @@ export default function Inner({ children }){
     const slide = {
         inital:{
             top:0,
+            opacity:1
         },
         enter:{
             top:"100%",
             transition: {
                 duration:1,
                 ease: [0.76, 0, 0.24, 1]
-            }
+            },
+            opacity:0
         },
-        exit: {
-            top:"-100%",
+        exit:{
+            top:0,
             transition:{
                 duration: 1,
                 ease: [0.76, 0, 0.24, 1]
-            }
+            },
+            opacity:1
         }
     }
     return(
