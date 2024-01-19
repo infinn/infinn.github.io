02ca7cdfe30e6f0b220de45e817cd3cd584361e8
infinn <71609708+infinn@users.noreply.github.com>
2024-01-19 16:00:15 -0400

    Update page.js

--- src/app/proyectos/[id]/page.js
+++ src/app/proyectos/[id]/page.js
@@ -1,17 +1,20 @@
 import  CreadorPost  from "../../components/creador-post/page"
 import "./style.css"
+import projectlist from '../../post.json'
 
 
 export async function generateStaticParams() {
-    const res = await fetch('https://infinn.github.io/post.json')
-    const posts = await res.json()
-    
-    return posts.map((post) => ({
-      id: post.id,
+    return projectlist.map((p) => ({
+        uniquepage: p.id.toString(),
     }))
 }
 export default function Page({params}){
-    const { id } = params
+    const project = projectlist.find(p => p.id.toString() === uniquepage)
+    if (!project) {
+        return(
+            <h1>error</h1>
+        )
+    }
     return(
         <>
             <section>
